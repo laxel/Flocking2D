aafdab8bf5f8ca0e55aae2de11e90d42ae22c5aa
Axel Karlsson <axeka@student.chalmers.se>
2020-02-10 12:09:33 +0100

    Added simple UI

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ ctx.canvas.width  = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
 
 // --- Parameters ---
-// * Boids
+
+var showStats = true;
+
+// Boids
 var boidSize = 15; // 20
 var boidAngle = 0.2 * Math.PI;
 var boidViewingAngle = 0.8 * Math.PI; // 0.8
@@ -30,41 +33,38 @@ canvas.onmousemove = function(e) {
 	mouseY = e.clientY;
 };
 
-function drawBoid(boid) {
-	var x = boid.x;
-	var y = boid.y;
-	var angle = boid.angle;
-
-	var x1 = x + (boidSize / 2) * Math.cos(angle);
-	var y1 = y - (boidSize / 2) * Math.sin(angle);
-	
-	var toBackLength = Math.sqrt(Math.pow(Math.tan(boidAngle/2)*boidSize,2) + Math.pow(boidSize/2,2));
-
-	var x2 = x + toBackLength * Math.cos(Math.PI - (boidAngle - angle));
-	var y2 = y - toBackLength * Math.sin(Math.PI - (boidAngle - angle));
-
-	var x3 = x - (boidSize / 4) * Math.cos(angle);
-	var y3 = y + (boidSize / 4) * Math.sin(angle);
-
-	var x4 = x + toBackLength * Math.cos(Math.PI + (boidAngle + angle));
-	var y4 = y - toBackLength * Math.sin(Math.PI + (boidAngle + angle));
-	
-
-	ctx.beginPath();
-	ctx.moveTo(x1, y1);
-	ctx.lineTo(x2, y2);
-	ctx.lineTo(x3, y3);
-	ctx.lineTo(x4, y4);
-	ctx.closePath();
-	ctx.fillStyle = "black";
-	ctx.fill();
-
-}
 
 function drawBoids() {
 	for (var i = 0; i < boids.length; i++) {
-		var b = boids[i];
-		drawBoid(b);
+		var boid = boids[i];
+
+		var x = boid.x;
+		var y = boid.y;
+		var angle = boid.angle;
+	
+		var x1 = x + (boidSize / 2) * Math.cos(angle);
+		var y1 = y - (boidSize / 2) * Math.sin(angle);
+		
+		var toBackLength = Math.sqrt(Math.pow(Math.tan(boidAngle/2)*boidSize,2) + Math.pow(boidSize/2,2));
+	
+		var x2 = x + toBackLength * Math.cos(Math.PI - (boidAngle - angle));
+		var y2 = y - toBackLength * Math.sin(Math.PI - (boidAngle - angle));
+	
+		var x3 = x - (boidSize / 4) * Math.cos(angle);
+		var y3 = y + (boidSize / 4) * Math.sin(angle);
+	
+		var x4 = x + toBackLength * Math.cos(Math.PI + (boidAngle + angle));
+		var y4 = y - toBackLength * Math.sin(Math.PI + (boidAngle + angle));
+		
+	
+		ctx.beginPath();
+		ctx.moveTo(x1, y1);
+		ctx.lineTo(x2, y2);
+		ctx.lineTo(x3, y3);
+		ctx.lineTo(x4, y4);
+		ctx.closePath();
+		ctx.fillStyle = (i == 0 && showStats == true) ? "red" : "black";
+		ctx.fill();
 	}
 }
 
@@ -244,6 +244,52 @@ function collision(b) {
 	return [0,0];
 }
 
+function drawStats(col,sep,ali,coh) {
+	var sum = 0;
+	var temp = [col,sep,ali,coh]
+	var result = [0,0,0,0];
+	for(var i = 0; i < temp.length; i++) {
+		temp[i] = temp[i] > 1 ? 1 : temp[i];
+
+		if(sum + temp[i] >= 1) {
+			result[i] = 1 - sum;
+			break;
+		}
+
+		result[i] = temp[i];
+		sum += temp[i];
+	}
+
+	var barWidth = 80;
+	var colors = ["#000000", "#e6ac00", "#4dff4d","#0033cc"];
+	var barHeightMax = 120;
+
+	// Draw bars
+	for(var i = 0; i < result.length; i++) {
+		var barHeight = barHeightMax * result[i];
+
+		ctx.beginPath();
+		ctx.fillStyle = colors[i];
+		ctx.fillRect(canvas.width-barWidth*(result.length-i), canvas.height-barHeight, barWidth, barHeight);
+		ctx.stroke();
+	}
+
+	ctx.font = "15px Arial";
+	ctx.fillStyle = "black";
+	ctx.fillText("Col", canvas.width-barWidth*3.7, canvas.height-barHeightMax-10);
+	ctx.fillText("Sep", canvas.width-barWidth*2.7, canvas.height-barHeightMax-10);
+	ctx.fillText("Ali", canvas.width-barWidth*1.7, canvas.height-barHeightMax-10);
+	ctx.fillText("Coh", canvas.width-barWidth*0.7, canvas.height-barHeightMax-10);
+
+
+	// Draw UI border
+	ctx.beginPath()
+	ctx.rect(canvas.width-barWidth*result.length, canvas.height-barHeightMax, barWidth*result.length,barHeightMax);
+	ctx.stroke();
+
+	return result;
+}
+
 function updateBoids() {
 	for (var i = 0; i < boids.length; i++) {
 		var b = boids[i];
@@ -259,6 +305,9 @@ function updateBoids() {
 		var ali = alignment(b, neigh);
 		var coh = cohesion(b, neigh);
 
+		// Draw UI for the first boid and how it is effected by the rules
+		if (i == 0 && showStats) drawStats(col[1],sep[1],ali[1],coh[1]);
+		
 		var rules = [col,sep,ali,coh];
 		var accumilator = 0;
 		var angleSum = 0;
@@ -300,20 +349,20 @@ function draw() {
 
 // --- SPAWN BOIDS ---
 // Spawn random boids
-for (var i = 0; i < 200; i++) {
+for (var i = 0; i < 100; i++) {
 	boids.push(new Boid(Math.random() * canvas.width, Math.random() * canvas.height
 						, Math.random() * Math.PI * 2))
 }
 
 // --- SPAWN WALLS ----
 // Wall cage
-/*
+
 createWall(300,300,500,100);
 createWall(500,100,700,100);
 createWall(700,100,900,500);
 createWall(900,500,500,800);
 createWall(500,800,300,300);
-*/
+
 
 
 // Wall box
@@ -333,6 +382,7 @@ createWall(0,canvas.height,0,0);
 */
 
 // Random walls
+/*
 for (var i = 0; i < 10; i++) {
 	var xDiff = Math.random() * 200 * (Math.random() > 0.5 ? 1:-1);
 	var yDiff = Math.random() * 200 * (Math.random() > 0.5 ? 1:-1);
@@ -340,6 +390,7 @@ for (var i = 0; i < 10; i++) {
 	var y = Math.random() * canvas.height;
 	createWall(x,y,x+xDiff,y+yDiff);
 }
+*/
 
 // --- Start simulation ---
 var iId = setInterval(draw,10);
